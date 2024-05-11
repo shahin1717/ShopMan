c9f4dfbd6b3e4a416988eb02977d9af8f4f44d9a
adile00 <151494791+adile00@users.noreply.github.com>
2024-05-11 19:15:51 +0400

    Update search1.c

--- search1.c
+++ search1.c
@@ -1083,18 +1083,15 @@ int main() {
     }
     fclose(inventoryFile);
     int choice;
-    displayMenu();
     do {
-        while(1){
-            if(scanf("%d", &choice)!=1){
-                printf("\033[31;1mInvalid input.\033[0m Please enter a number!\nYour choice: ");
-                scanf("%*[^\n]"); // Clear input buffer
-                scanf("%*c");
+    displayMenu();
+        scanf("%d", &choice);
+        while(choice>9 || choice<1){
+                printf("\033[31;1mInvalid choice.\033[0m Please try again.\n");
+                printf("Enter your choice: ");
+                scanf("%*[^\n]"); // clear input buffer
+                scanf("%d", &choice);
             }
-            else{
-                break;
-            }
-        }
         getchar(); // Consume the newline character
 
         switch (choice) {
